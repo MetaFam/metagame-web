24900838274bccd6e0773742caa288769490ac48
Hammad Jutt <jutt@ualberta.ca>
2020-02-22 23:25:41 -0700

    Setup Ethers and Web3 providers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/core';
-import { Layout } from '../components/Layout';
+import { Box, Grid, Heading } from '@chakra-ui/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from '@web3-react/core';
+import React, { useEffect, useState } from 'react';
 
-export default function IndexPage(): JSX.Element {
+import { Layout } from '../components/Layout';
+import { useEagerConnect, useInactiveListener } from '../lib/hooks';
+import { formatEthAddress } from '../lib/utils';
+
+export default function Index(): JSX.Element {
+  const { connector, account } = useWeb3React<Web3Provider>();
+
+  // handle logic to recognize the connector currently being activated
+  const [activatingConnector, setActivatingConnector] = useState();
+  useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
+  }, [activatingConnector, connector]);
+
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
+  const triedEager = useEagerConnect();
+
+  // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+  useInactiveListener(!triedEager || !!activatingConnector);
+
   return (
     <Layout>
-      <Heading>Hello MetaGame!</Heading>
+      <Grid templateColumns="1fr minmax(auto, 1000px) 1fr" gap={6}>
+        <Box />
+        <Box>
+          <Heading>Hello {formatEthAddress(account)}!</Heading>
+        </Box>
+        <Box />
+      </Grid>
     </Layout>
   );
 }
